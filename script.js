8bebb50e51a00d1da9776c01af1a5b2078faa9fc
Anya Alesik <nayavii17@gmail.com>
2024-02-08 19:58:47 +0100

    added accordion

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ new Swiper ('.swiper',{
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+const accordionTitles = document.querySelectorAll(".accordionTitle");
+
+accordionTitles.forEach((accordionTitle) => {
+  accordionTitle.addEventListener("click", () => {
+    if (accordionTitle.classList.contains("is-open")) {
+      accordionTitle.classList.remove("is-open");
+    } else {
+      const accordionTitlesWithIsOpen = document.querySelectorAll(".is-open");
+      accordionTitlesWithIsOpen.forEach((accordionTitleWithIsOpen) => {
+        accordionTitleWithIsOpen.classList.remove("is-open");
+      });
+      accordionTitle.classList.add("is-open");
+    }
+  });
+});
